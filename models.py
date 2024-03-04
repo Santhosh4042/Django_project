4b422aca3315711fc45080eb543239f2028043e1
Santhosh4042 <114553750+Santhosh4042@users.noreply.github.com>
2024-03-04 12:44:08 +0430

    Delete models.py

--- models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class DEPT(models.Model):
-    DEPTNO=models.IntegerField(max_length=10)
-    DEPTNAME=models.CharField(max_length=10)
-    DEPTLOC=models.CharField(max_length=10)
-    
